8c02fa22d8cbf12c9a3b011e08426c595b7a9748
Raziq <razipanangatuthodi@gmail.com>
2026-03-18 17:37:21 +0530

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(router)
 app.use('/uploads',express.static('./uploads'))
 
-const port = 3000
+const port = process.env.PORT || 3000
 app.listen(port,()=>{
     console.log(`server running on${port}`)
 })
